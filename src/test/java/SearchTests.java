454edd47ba7016477e3dfd47d780603620da0e83
Pavel Aleksanochkin <soker.96@mail.ru>
2025-01-15 14:34:49 +0300

    Добавлен .gitignore

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -1,6 +1,5 @@
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
-
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
